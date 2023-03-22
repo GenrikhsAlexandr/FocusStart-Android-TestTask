9f66a3d0d9b4f6418b5e2d6bc4af23d606da22b3
Aleksandr <aleksandr.genrikhs@gmail.com>
2023-03-22 17:14:27 +1100

    add data

--- app/src/main/java/com/genrikhsalexandr/focusstart_android_testtask/data/api/BinService.kt
+++ app/src/main/java/com/genrikhsalexandr/focusstart_android_testtask/data/api/BinService.kt
@@ -3,13 +3,10 @@ package com.genrikhsalexandr.focusstart_android_testtask.data.api
 import com.genrikhsalexandr.focusstart_android_testtask.data.pojo.Example
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface BinService {
 
     @GET("{}")
     suspend fun getBinInfo(
         @Path("") number: Int): Example
-
-
 }
